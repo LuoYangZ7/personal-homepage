2a37b7a4929f4c1394a084f82669a1cfc9ff9b98
LuoYangZ7 <2641157027@qq.com>
2023-10-22 17:11:46 +0800

    项目初始化
    项目初始化

---
+++ personal-homepage-backend/src/main/java/com/lz7/PersonalHomepageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lz7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalHomepageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalHomepageBackendApplication.class, args);
+    }
+
+}
---
+++ personal-homepage-backend/src/test/java/com/lz7/PersonalHomepageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalHomepageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
